46bb16dccada2f0097eb0fd116b2f934be9421b1
beanstown106 <nbenis106@gmail.com>
2015-12-08 15:42:37 -0500

    angler: add missing drm prop

--- device.mk
+++ device.mk
@@ -390,6 +390,10 @@ $(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-hwui-memory.m
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/phone-xxxhdpi-3072-dalvik-heap.mk)
 
+# drmservice prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
 # facelock properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.black_timeout=400 \
